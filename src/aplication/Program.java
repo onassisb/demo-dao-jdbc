397c8bc85cc42710c248d855a47ef5f8db9cb8a7
Onassis Toledo <onassisb@gmail.com>
2020-11-02 18:56:15 -0300

    Insert do Seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -3,11 +3,14 @@ package aplication;
 import model.dao.DaoFactory;
 import model.entities.Seller;
 
+import java.util.Date;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("=== TEST 1: Seller findById ===");
         var sellerDao = DaoFactory.createSellerDao();
-        System.out.println(sellerDao.findById(3));
+        var seller0 = sellerDao.findById(3);
+        System.out.println(seller0);
 
         System.out.println("=== TEST 2: Seller for Department findById ===");
         var sellers = sellerDao.findByDepartment(1);
@@ -16,5 +19,10 @@ public class Program {
         System.out.println("=== TEST 3: Seller findAll ===");
         sellers = sellerDao.findAll();
         for (Seller seller : sellers) System.out.println(seller);
+
+        System.out.println("=== TEST 4: Seller Insert ===");
+        var seller = new Seller(null, "Jonh Smith", "jsmith@gmail.com", new Date(), 4000.00, seller0.getDepartament());
+        //sellerDao.insert(seller);
+        System.out.println(seller);
     }
 }
