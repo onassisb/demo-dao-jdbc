d5188236ec06e5c9f82807b47be3e4e842bd292e
Onassis Toledo <onassisb@gmail.com>
2020-11-02 11:06:20 -0300

    Refatoracao FindById do Seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -4,6 +4,7 @@ import model.dao.DaoFactory;
 
 public class Program {
     public static void main(String[] args) {
+        System.out.println("=== TEST 1: Seller findById ===");
         var sellerDao = DaoFactory.createSellerDao();
         System.out.println(sellerDao.findById(3));
     }
